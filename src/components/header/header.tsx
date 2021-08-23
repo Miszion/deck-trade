1d163d7b7323ab0a3dff3104fe16ab1822467c08
Mission Marcus <missionmarcus@gmail.com>
2021-08-22 22:35:18 -0500

    added header styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,11 +2,15 @@ import './header.scss'
 import Logo from '../../assets/logos/logo'
 import { Link } from 'react-router-dom'
 import Button from '../button/button'
+import { useState } from 'react'
 
 const Header = (props: any) => {
+
+    const [menu, toggleMenu] = useState(false)
+
     return (
         <div className='header'>
-            <div className='header-container'>
+            <div className='header-container-desktop'>
                 <div className='header-logo-container'>
                     <Link to='/' className='logo-link'>
                         <Logo className='header-logo' color="#20232a"></Logo>
@@ -19,6 +23,27 @@ const Header = (props: any) => {
                         <Link to='/' className='header-item'>Account</Link>
                     </div>
                     <Button color="#ffffff" textColor="#20232a" text="Sign Up"></Button>
+                </div>
+            </div>
+            <div className='header-container-mobile'>
+                <div className='header-hamburger-container'>
+                    <div className='header-hamburger' onClick={() => {toggleMenu(!menu)}}>
+                        {[0,1,2].map((i) => 
+                            <div className='hamburger-line' key={i}/>
+                        )}
+                    </div>
+                </div>
+                <div className='center-logo-container'>
+                    <Link to='/' className='logo-link'>
+                        <Logo className='header-logo' color="#20232a"></Logo>
+                    </Link>
+                </div>
+            </div>
+            <div className='side-menu' style={{left: menu ? '0px': '-250px'}}>
+                <div className='menu-items-mobile'>
+                    <Link to='/' className='header-item'>Trade</Link>
+                    <Link to='/' className='header-item'>Company</Link>
+                    <Link to='/' className='header-item'>Account</Link>
                 </div>
             </div>
         </div>
